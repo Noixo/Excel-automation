bdc87f10e110674d7093d05fb5d2b797ec6c4f3e
Matthew Whiteley <flatout12@hotmail.com>
2020-04-07 01:52:46 +1000

    V1.14: finalised door count code, added support for salesperson data to be saved in wrong spot.

--- DoorCountAutomation/DoorCountAutomation/Program.cs
+++ DoorCountAutomation/DoorCountAutomation/Program.cs
@@ -11,15 +11,41 @@ namespace SeleniumTest
 {
     class Program
     {
+        public void doorCount(String kk)
+        {
+ 
+
+            StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + "\\DoorCount.txt");
+            if (kk == "" || kk == "0")
+            {
+                sw.WriteLine("1");
+            }
+            else
+            {
+                sw.WriteLine("0");  //door count obtained
+            }
+            sw.WriteLine(kk);   //door count
+                                //File.WriteAllLines(sw, );
+                                //sw.WriteLine(1);   //door count recorded
+            Console.Write("Door Count: " + kk);
+
+            //Close the file
+            sw.Close();
+        }
+
         static void Main(string[] args)
         {
+            String kk = "";
+            Program p = new Program();
+
             //driver.HideCommandPromptWindow = true;
-            // navigate to URL  "https://l.vemcount.com/embed/pane/xIgeG9iTUypChTb"
+            // navigate to URL  "https://l.vemcount.com/embed/pane/xIgeG9iTUypChTbt" 'croydon
+
             if (args.Length == 0)
             {
                 Console.WriteLine("Arguments empty. Aborting program.");
                 System.Environment.Exit(1);
-            }
+            } 
 
             Console.WriteLine("Attempting to grab door count. ");
 
@@ -31,33 +57,44 @@ namespace SeleniumTest
             //options.Proxy = null;
             IWebDriver driver = new ChromeDriver(new ChromeOptions { Proxy = null });
 
-            //string driverPath = @"C:\where ever the driver it\";
-            //var chromeOptions = new ChromeOptions();
-            //chromeOptions.Proxy = null;
-            //IWebDriver browser = new ChromeDriver(driverPath, chromeOptions);
-
-
             driver.Navigate().GoToUrl(args[0]);
 
-            Thread.Sleep(3000);
+            DateTimeOffset startTime = DateTimeOffset.Now;
 
-            String kk = driver.FindElement(By.CssSelector("div.flex")).GetAttribute("innerHTML");
+            while (true)
+            {
+                try
+                {
+                    kk = driver.FindElement(By.CssSelector("div.flex")).GetAttribute("innerHTML");
+                    if (kk != "")
+                    {
+                        int tmp = kk.IndexOf("<span>") + 6;
+                        int test = kk.IndexOf("</span>\r\n");
 
-            int tmp = kk.IndexOf("<span>") + 6;
-            int test = kk.IndexOf("</span>\r\n");
+                        kk = kk.Substring(tmp, test - tmp);
+                        p.doorCount(kk);
+                        //p.doorCount(kk);
+                        //close the browser  
+                        driver.Quit();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.Write("Cannot find door count.");
+                    kk = "";
+                    p.doorCount(kk);
+                }
 
-            kk = kk.Substring(tmp, test - tmp);
+                if (DateTimeOffset.Now.Subtract(startTime).TotalMilliseconds > 10000)
+                    throw new TimeoutException();
+            }
 
-            StreamWriter sw = new StreamWriter(Directory.GetCurrentDirectory() + "\\DoorCount.txt");
-            sw.WriteLine(kk);   //door count
-            //File.WriteAllLines(sw, );
-            //sw.WriteLine(1);   //door count recorded
-            Console.Write("Door Count: " + kk);
-            //Close the file
-            sw.Close();
+            //Thread.Sleep(3500);
+            //while 
+            
 
-            //close the browser  
-            driver.Quit();
+
         }
     }
+ 
 }
