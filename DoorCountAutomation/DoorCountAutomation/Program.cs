1dc5ff02e675cc1274d5e0320b62984a125d179d
Matthew Whiteley <flatout12@hotmail.com>
2020-04-12 21:39:35 +1000

    Added dependencies to doorcount code.

--- DoorCountAutomation/DoorCountAutomation/Program.cs
+++ DoorCountAutomation/DoorCountAutomation/Program.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+
+using System.Diagnostics;
 namespace SeleniumTest
 {
     class Program
@@ -32,15 +34,35 @@ namespace SeleniumTest
             //Close the file
             sw.Close();
         }
+        
 
         static void Main(string[] args)
         {
             String kk = "", newCount = "";
             Boolean stable = false;
             Program p = new Program();
+            IWebDriver driver;
+
+            // ChromeDriver is just AWFUL because every version or two it breaks unless you pass cryptic arguments
+            //AGRESSIVE: options.setPageLoadStrategy(PageLoadStrategy.NONE); // https://www.skptricks.com/2018/08/timed-out-receiving-message-from-renderer-selenium.html
+            ChromeOptions options = new ChromeOptions();
+            //options.AddArguments("start-maximized"); // https://stackoverflow.com/a/26283818/1689770
+            options.AddArguments("enable-automation"); // https://stackoverflow.com/a/43840128/1689770
+            //options.AddArguments("--headless"); // only if you are ACTUALLY running headless
+            options.AddArguments("--no-sandbox"); //https://stackoverflow.com/a/50725918/1689770
+            options.AddArguments("--disable-infobars"); //https://stackoverflow.com/a/43840128/1689770
+            options.AddArguments("--disable-dev-shm-usage"); //https://stackoverflow.com/a/50725918/1689770
+            options.AddArguments("--disable-browser-side-navigation"); //https://stackoverflow.com/a/49123152/1689770
+            options.AddArguments("--disable-gpu"); //https://stackoverflow.com/questions/51959986/how-to-solve-selenium-chromedriver-timed-out-receiving-message-from-renderer-exc
+            options.AddArguments("enable-features=NetworkServiceInProcess");
+
+            //options.AddArguments(PageLoadStrategy.None);
+            options.PageLoadStrategy = PageLoadStrategy.Normal;
+
+            driver = new ChromeDriver(options);
 
             //driver.HideCommandPromptWindow = true;
-            // navigate to URL  "https://l.vemcount.com/embed/pane/xIgeG9iTUypChTbt" 'croydon
+            // navigate to URL  "https://l.vemcount.com/embed/pane/xIgeG9iTUypChTb" 'croydon
 
             if (args.Length == 0)
             {
@@ -53,13 +75,13 @@ namespace SeleniumTest
             //create the reference for the browser  
             //IWebDriver driver = new ChromeDriver();
 
-            ChromeOptions options = new ChromeOptions();
+            //ChromeOptions options = new ChromeOptions();
             options.AddArgument("--log-level=3");
             //options.Proxy = null;
-            IWebDriver driver = new ChromeDriver(new ChromeOptions { Proxy = null });
+            //IWebDriver driver = new ChromeDriver(new ChromeOptions { Proxy = null });
 
             driver.Navigate().GoToUrl(args[0]);
-
+            //Thread.Sleep(600);
             DateTimeOffset startTime = DateTimeOffset.Now;
 
             while (true)
@@ -93,6 +115,8 @@ namespace SeleniumTest
                             if (kk == newCount)
                             {
                                 p.doorCount(kk);
+                                Console.Write("Success");
+                                Debug.WriteLine("Success ", kk);
                                 driver.Quit();
                                 System.Environment.Exit(1);
                             }
