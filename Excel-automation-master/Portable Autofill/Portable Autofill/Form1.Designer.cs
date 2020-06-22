16b56f392147f7331f6a4aff3237c3e3e3aa2041
Matthew Whiteley <flatout12@hotmail.com>
2020-06-22 21:58:43 +1000

    V1.17: Support for other stores

---
+++ Excel-automation-master/Portable Autofill/Portable Autofill/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Portable_Autofill
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.ButtonAutoFill = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// ButtonAutoFill
+			// 
+			this.ButtonAutoFill.Location = new System.Drawing.Point(183, 114);
+			this.ButtonAutoFill.Name = "ButtonAutoFill";
+			this.ButtonAutoFill.Size = new System.Drawing.Size(75, 23);
+			this.ButtonAutoFill.TabIndex = 0;
+			this.ButtonAutoFill.Text = "AutoFill";
+			this.ButtonAutoFill.UseVisualStyleBackColor = true;
+			this.ButtonAutoFill.Click += new System.EventHandler(this.ButtonAutoFill_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(384, 71);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "EOD";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(384, 12);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 2;
+			this.button3.Text = "Monthly";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// openFileDialog2
+			// 
+			this.openFileDialog2.FileName = "openFileDialog2";
+			this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog2_FileOk);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(101, 12);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(277, 20);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(101, 73);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(277, 20);
+			this.textBox2.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.ClientSize = new System.Drawing.Size(470, 149);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.ButtonAutoFill);
+			this.Name = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonLinkMonthlyStats;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.Button buttonRunAutoFill;
+		private System.Windows.Forms.Button buttonLinkEOD;
+		private System.Windows.Forms.Button ButtonAutoFill;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.OpenFileDialog openFileDialog2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TextBox textBox2;
+	}
+}
+
---
+++ Excel-automation-master/Portable Autofill/Portable Autofill/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Portable_Autofill
+{
+	public partial class Form1 : Form
+	{
+		public String getMonth;
+		public String getEOD;
+
+		public String returnGetMonth()
+		{
+			return textBox1.Text;//getMonth;
+		}
+		public String returnGetEOD()
+		{
+			return textBox2.Text;
+		}
+		public Form1()
+		{
+			InitializeComponent();
+		}
+		//Autofill
+		private void ButtonAutoFill_Click(object sender, EventArgs e)
+		{
+			ExcelInteract fill = new ExcelInteract();
+			fill.autoFill(textBox1.Text, textBox2.Text);
+
+		}
+		//Monthly loc
+		private void button3_Click(object sender, EventArgs e)
+		{
+			DialogResult result = openFileDialog2.ShowDialog();
+
+			if (result == DialogResult.OK) // Test result.
+			{
+				string file = openFileDialog2.FileName;
+				textBox1.Text = file;
+				getMonth = file;
+				Console.WriteLine(file);
+			}
+			//Console.WriteLine(result);
+		}
+		//EOD loc
+		private void button2_Click(object sender, EventArgs e)
+		{
+			DialogResult result = openFileDialog2.ShowDialog();
+			if (result == DialogResult.OK) // Test result.
+			{
+				string file = openFileDialog2.FileName;
+				textBox2.Text = file;
+				getEOD = file;
+				Console.WriteLine(file);
+			}
+			//Console.WriteLine(result);
+		}
+
+		private void openFileDialog2_FileOk(object sender, CancelEventArgs e)
+		{
+
+		}
+
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
